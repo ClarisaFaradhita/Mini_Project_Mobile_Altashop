510c4c834dfd84a443f72ad5cf0054c4ed67e97e
ClarisaFaradhita <clarisafaradhita8@gmail.com>
2022-05-21 10:44:42 +0700

    push pertama

---
+++ Test Suites/Negative/Test Suite_Negative.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite_Negative</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e6e6881f-46ca-48f2-bff4-6d8ba4b85092</testSuiteGuid>
+   <testCaseLink>
+      <guid>0f7b90eb-8dad-478c-961b-e6b868565164</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Negatif/TC01_Negative_Register</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>297505a9-6cb2-41ad-a963-dae61c5e8235</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Negatif/TC02_Negative_Login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Positif/Test Suite Positif.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite Positif</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2a1e6149-62ec-4a62-a501-5c343508b90d</testSuiteGuid>
+   <testCaseLink>
+      <guid>318369cc-e8d4-4c72-acf8-7714c4b61be6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Register/TC01_Register</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f4f3359f-a848-44fc-a94e-62eb31ddac4c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/TC02_Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>38b9d467-1d14-4eb6-bb97-dfa69a61d358</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Add_To_Cart/TC04_Cart</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
